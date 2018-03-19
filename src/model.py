1293e1e42b13527eb8b4792268e399a32335bb81
LincolnZjx <lincolnz9511@gmail.com>
2018-03-19 19:40:54 +0800

    update

--- src/model.py
+++ src/model.py
@@ -185,7 +185,8 @@ class Model():
 
     def get_memory(self):
         cls = memory.LSHMemory if self.use_lsh else memory.Memory
-        return cls(self.rep_dim, self.memory_size, self.vocab_size, nn_device='/gpu:0')
+        return cls(self.rep_dim, self.memory_size, self.vocab_size,
+                   caching_device='/gpu:0',nn_device='/gpu:0')
 
     def get_classifier(self):
         return BasicClassfier(self.output_dim)
